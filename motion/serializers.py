14b225f4111d066400e8e56c4e1a2298c440e0d1
Maulana M Hafidz <maulanasmd123@gmail.com>
2024-11-29 22:04:57 +0800

    Fix Bug API

--- motion/serializers.py
+++ motion/serializers.py
@@ -4,4 +4,4 @@ from .models import Membership
 class MembershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Membership
-        fields = ['id', 'name', 'price', 'duration']  # Tambahkan field sesuai model Membership
+        fields = ['id', 'name', 'price', 'duration', 'limit']  # Tambahkan field sesuai model Membership
